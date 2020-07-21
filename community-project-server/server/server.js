9b535626fe5719dd9aac3341cce36261e0c71502
alex.burgess3 <alex.burgess3@tafensw.edu.au>
2020-07-21 16:52:27 +1000

    Initial Commit

---
+++ community-project-server/server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+var cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
+const storage = require('node-persist');
+const projects = require('../projects.json');
+const { getItem } = require('node-persist');
+const server = express();
+server.use(express.json());
+server.use(bodyParser.json());
+server.use(cors());
+const port = 3000;
+
+(async()=>{
+    await storage.init({dir: "./data"});
+
+    server.get("/api/projects", async(req,res)=>{  
+        res.json(await storage.valuesWithKeyMatch(/project-/));   
+    });
+
+    server.post("/api/projects/submit", async(req,res)=>{
+        let data = req.body;
+        let project = {
+            id: uuidv4(),
+            ...data,
+            submissionDate: new Date().toISOString().slice(0,10)
+        }
+        await storage.setItem(`project-${project.id}`,project);
+        res.json({status:200,msg:"Thank you for your submission, we will treat it as Revenue NSW would."});
+    });
+
+    server.listen(port, () => console.log(`Server listening at port ${port}`));
+})();
