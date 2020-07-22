47799dbc11bcb3abcd95b23638e19284b93dd924
Vijaylakshmi <Vijaylakshmi.Govindraj@service.nsw.gov.au>
2020-07-22 10:23:45 +1000

    added validate.js file and updated server.js

--- community-project-server/server/server.js
+++ community-project-server/server/server.js
@@ -4,30 +4,65 @@ var cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
 const storage = require('node-persist');
 const projects = require('../projects.json');
-const { getItem } = require('node-persist');
 const server = express();
 server.use(express.json());
 server.use(bodyParser.json());
 server.use(cors());
 const port = 3000;
+const { validateGivenName, validatePostcode } = require('./validate');
 
-(async()=>{
-    await storage.init({dir: "./data"});
-// server side
-    server.get("/api/projects", async(req,res)=>{  
-        res.json(await storage.valuesWithKeyMatch(/project-/));   
+(async () => {
+    await storage.init({ dir: "./data" });
+
+    //display all the projects available in data
+    //http://localhost:3000/api/projects
+    server.get("/api/projects", async (req, res) => {
+        res.json(await storage.valuesWithKeyMatch(/project-/));
     });
 
-    server.post("/api/projects/submit", async(req,res)=>{
+
+     //submit a project and return a message 
+    //http://localhost:3000/api/projects/submit
+    server.post("/api/projects/submit", async (req, res) => {
         let data = req.body;
+        let givenName = req.body.givenName;
+        let postcode = req.body.postcode;
+        let description =req.body.description;
+        if (!validatePostcode(postcode)) {
+            res.json({ status: 500, error: "incorrect postcode" });
+        }
+        if (!validateGivenName(givenName)) {
+            res.json({ status: 500, error: "Given name can only have 50 charecters" });
+        }
+        if (!Description(description)){
+            res.json({status:500, error: "Description should not exceed 300 charecters"});
+        }
         let project = {
             id: uuidv4(),
             ...data,
-            submissionDate: new Date().toISOString().slice(0,10)
+            submissionDate: new Date().toISOString().slice(0, 10)
         }
-        await storage.setItem(`project-${project.id}`,project);
-        res.json({status:200,msg:"Thank you for your submission, we will treat it as Revenue NSW would."});
+
+        //Display only the eligible projects for Public 
+        //http://localhost:3000/api/projects/eligible
+        server.get("/api/projects/eligible", async (req, res) => {  //incorrect working on it
+            res.json({data: status == true, status: 200})
+        });
+
+        //Display only the pending projects for administrator
+        //http://localhost:3000/api/projects/pending      
+        server.get("/api/projects/eligible", async (req, res) => {    //incorrect working on it
+            res.json({data: status == true, status: 200})
+        });
+
+
+        await storage.setItem(`project-${project.id}`, project);
+        res.json({ status: 200, msg: "Thank you for your submission, we will treat it as Revenue NSW would." });
+
     });
+
+
+
 
     server.listen(port, () => console.log(`Server listening at port ${port}`));
 })();
---
+++ community-project-server/server/validate.js
@@ -0,0 +1,24 @@
+module.exports = {
+
+    validateGivenName: function (givenName) {
+        if (givenName.length < 1 || givenName.length > 50) {
+            return false;
+        }
+
+        return true;
+    },
+    
+    validatePostcode: function (postcode) {
+        if (Number(postcode[0]) != 2 && Number(postcode.length) != 4) {
+            return false;
+        }
+        return true;
+    },
+
+    validateDescription: function (description) {
+        if (description.length > 300){
+            return false;
+        }
+        return true;
+    }
+}
