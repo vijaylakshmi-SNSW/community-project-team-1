630e3d29c11a76cf70601412e74f45008c63dbc4
alex.burgess3 <alex.burgess3@tafensw.edu.au>
2020-07-23 09:43:17 +1000

    submit project functionality completed

--- community-project-client/src/component/projectsRegistry/ProjectsSubmitRegistry.js
+++ community-project-client/src/component/projectsRegistry/ProjectsSubmitRegistry.js
@@ -7,11 +7,41 @@ export default function ProjectsSubmitRegistry(props) {
     const [postcode, setPostcode] = useState("");
     const [description, setDescription] = useState("");
 
-    return(
+    const handleSubmitButton = e => {
+        e.preventDefault();
+
+        let project = {
+            givenName = givenName,
+            surname = surname,
+            postcode = postcode,
+            description = description
+        }
+
+        fetch('http://localhost:3000/api/projects/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(project),
+        })
+            .then(response => response.json())
+            .then(json => {
+                props.submitProject(json.project);
+            });
+    }
+
+    return (
         <div>
             <form>
-                <label>Given Name/s</label>
-                <input placeholder="Enter Given Name here"></input>
+                <label>Given Name/s:</label>
+                <input placeholder="Enter Given Name here" onChange={e => setGivenName(e.target.value)}></input>
+                <label>Surname:</label>
+                <input placeholder="Enter Surname here" onChange={e => setSurname(e.target.value)}></input>
+                <label>Postcode:</label>
+                <input placeholder="Enter Postcode here" onChange={e => setPostcode(e.target.value)}></input>
+                <label>Description of Project:</label>
+                <input placeholder="Enter Description here" onChange={e => setDescription(e.target.value)}></input>
+                <button onClick={handleSubmitButton}>Submit</button>
             </form>
         </div>
     );
