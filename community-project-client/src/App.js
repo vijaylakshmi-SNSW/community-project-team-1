cc99abbc6ac557209498259540efee2325cb0674
alex.burgess3 <alex.burgess3@tafensw.edu.au>
2020-07-24 14:38:41 +1000

    submit works w/o validation

--- community-project-client/src/App.js
+++ community-project-client/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./component/Home";
 import Nav from "./component/Nav";
 import ViewAll from "./component/projectsRegistry/ViewProjects";
-import ProjectsSubmitRegistry from "./component/projectsRegistry/ProjectsSubmitRegistry";
+import ProjectsRegistry from "./component/projectsRegistry/ProjectsRegistry";
 
 function App() {
 
@@ -14,7 +14,7 @@ function App() {
         <Nav />
         <Switch>
           <Route path="/home" component={Home} />
-          <Route path="/project-registry-submit" component={ProjectsSubmitRegistry} />
+          <Route path="/project-registry-submit" component={ProjectsRegistry} />
           <Route path="/view-all" component={ViewAll} />
         </Switch>
       </Router>
