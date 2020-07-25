50d99fd22a91a1e5fc7fcf927631e2565876999c
Aileen Gallacher <aileen.gallacher@tafensw.edu.au>
2020-07-25 23:45:37 +1000

    Added get functionality/vote buttons

--- community-project-client/src/component/projectsRegistry/ViewProjects.js
+++ community-project-client/src/component/projectsRegistry/ViewProjects.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function ViewProjects() {
 
 
+    const [projects, setProjects] = useState([]);
 
-    return(
+    useEffect(() => {
+        fetch("http://localhost:4000/api/projects/admin")
+            .then(res => res.json())
+            .then(projects => setProjects(projects));
+    }, []);
+
+    function handleVoteClick() {
+        return alert(`Thank you for your vote!`);
+    }
+
+
+    return (
         <div>
-            
+            <div className="viewall-container">
+                <div className="eligible-projects-image">
+                    <img src="https://www.nsw.gov.au/sites/default/files/styles/content_x1/public/2020-02/My-Community-Project-Eligible-Projects-DataCard-Banner-v04.png?itok=TUeWyMMC"></img>
+                </div>
+            </div>
+            <h1>Community Project</h1>
+            <p>Browse the projects that have been approved for consideration.</p>
+            <p>New project submissions are still being accepted. </p>
+
+            {projects.map(p =>
+                <div >
+                    {p.status === "approved" &&
+                        <div style={{ padding: "5px", margin: "5px", border: "1px solid grey" }}>
+                            <p><strong>{p.title}</strong></p>
+                            <p>{p.description}</p>
+                            <div style={{align: "right"}}>
+                                <button class="vote-button" onClick={handleVoteClick}>Vote for this project</button>
+                            </div>
+                        </div>}
+                </div>)}
         </div>
     );
 }
