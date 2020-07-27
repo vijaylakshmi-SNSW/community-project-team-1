586c0466f3f62baffe57a9f16e524852c8795f26
Vijaylakshmi <Vijaylakshmi.Govindraj@service.nsw.gov.au>
2020-07-28 07:03:32 +1000

    Updated Result.js

--- community-project-client/src/component/Admin.js
+++ community-project-client/src/component/Admin.js
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Result from './Result';
 
 export default function Admin() {
     const [status, setStatus] = useState('');
-
-
+    
     const [result, setResult] = useState([]);
 
+    
 
 
     function getUserRequest(e) {
-
+debugger;
         //stop the form from submitting and making a synchronous post request
         e.preventDefault();
         //instead we are making an asynchronous request using fetch
-
+        
         fetch(`http://localhost:4000/api/projects/${status}`, {
             headers: { 'Content-Type': 'application/json' },
 
@@ -27,17 +27,19 @@ export default function Admin() {
     
 
     return (
-        <div>
-            <label>Please select a status to view projects:</label>
-            <select name='status' onChange={(e) => setStatus(e.target.value)}>
-                <option value="">select a status </option>
+        <div >
+           
+            <label style={{'padding': '10px'}}>Please select a status to view projects:</label>
+            
+            <select style={{'padding': '10px'}} name='status' onChange={(e) => setStatus(e.target.value)}>
+                <option value="">Select a status </option>
                 <option value="approved">Approved</option>
                 <option value="rejected">Declined</option>
                 <option value="pending">Pending</option>
             </select>
 
-            <button onClick={getUserRequest}>View Projects</button>
-           
+            <button style={{'padding': '10px', 'margin': '10px '}} onClick={getUserRequest}>View Projects</button>
+            
             <div>
                 <br />
                 <br />
--- community-project-client/src/component/Result.js
+++ community-project-client/src/component/Result.js
@@ -1,67 +1,71 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
+import '../App.css';
 
 // const [status, setStatus] = useState('');
 //const [resultObj, setResultObj] = useState(undefined);
 
 export default function Result(props) {
-    const [answer, setAnswer] = useState([]);
-    
+    const [answer, setAnswer] = useState(undefined);
 
     let result = props.display;
-    
-    function sendUserRequest(e,result){
+
+    function sendUserRequest(e, result) {
         // debugger;
-        e.preventDefault(); 
+        e.preventDefault();
         let status = e.target.value;
         let id = result.id;
-        
-        let s = {status, id}
+        let s = { status, id }
         fetch("http://localhost:4000/api/projects/status/update", {
             method: 'PUT',
             headers: { 'Content-Type': 'application/json' },
-            body : JSON.stringify(s)
+            body: JSON.stringify(s)
         })
             .then((response) => response.json())
-            .then(json => setAnswer(json))
+            .then(json => setAnswer(json.data))
 
-     }
+    }
+    useEffect(() => {
 
-     
+        fetch('/api/projects')
+          .then(response => response.json())
+          .then(data => setAnswer(data));
+      }, []);
+    
+
+
+
 
     return (
         <div style={{ 'border': '2px', 'borderStyle': 'groove' }}>
             <br />
-        Name: {result.givenName}  {result.lastName}
+       <strong>Name:</strong>  {result.givenName}  {result.lastName}
             <br />
-        Title: {result.title}
+        <strong>Title:</strong> {result.title}
             <br />
-        Description: {result.description}
+        <strong>Description:</strong>{result.description}
             <br />
-         Status: {result.status}      
-         
-            {  
-                (result.status === 'pending' && 
-                    
+         <strong>Status:</strong>{result.status}
+
+            {
+                (result.status === 'pending' &&
+
                     <div>
                         <br />
-                        <button onClick={(event)=>sendUserRequest(event,result)} value="approved">Approve</button> 
+                        <button class="approve-button" onClick={(event) => sendUserRequest(event, result)} value="approved">Approve</button>
 
-                
-                       <button onClick= {(event) => sendUserRequest(event, result)} value= "rejected">Reject</button>
+
+                        <button class="reject-button" onClick={(event) => sendUserRequest(event, result)} value="rejected">Reject</button>
                         <br />
-                    </div> 
-                 )
-          
+                    </div>
+                )
+
             }
 
             <div>
-                {answer}
-                    
-                
-                
-            </div>
-      
 
+                {answer && <p>The project has been {answer.value.status}</p>}
+
+            </div>
             <br />
             <br />
         </div>
