1f46b720a57d7650b1b7cfa5c372eff672271fe4
Aileen Gallacher <aileen.gallacher@tafensw.edu.au>
2020-07-27 16:09:50 +1000

    minor changes to ViewProjects - added <li>

--- community-project-client/src/component/projectsRegistry/ViewProjects.js
+++ community-project-client/src/component/projectsRegistry/ViewProjects.js
@@ -6,7 +6,7 @@ export default function ViewProjects() {
     const [projects, setProjects] = useState([]);
 
     useEffect(() => {
-        
+
         fetch("http://localhost:4000/api/projects/approved")
             .then(res => res.json())
             .then(projects => setProjects(projects.data));
@@ -14,14 +14,14 @@ export default function ViewProjects() {
 
     function handleVoteClick() {
         return alert(`Thank you for your vote!`);
-    }
+        }
 
 
     return (
         <div className="viewprojects-eligible">
             <div className="viewall-container">
                 <div className="eligible-projects-image">
-                    <img src="https://www.nsw.gov.au/sites/default/files/styles/content_x1/public/2020-02/My-Community-Project-Eligible-Projects-DataCard-Banner-v04.png?itok=TUeWyMMC"></img>
+                    <img src="https://www.nsw.gov.au/sites/default/files/styles/content_x1/public/2020-02/My-Community-Project-Eligible-Projects-DataCard-Banner-v04.png?itok=TUeWyMMC" alt="Community Project"></img>
                 </div>
             </div>
             <h1>Community Project</h1>
@@ -29,15 +29,17 @@ export default function ViewProjects() {
             <p>New project submissions are still being accepted. </p>
 
             {projects.map(p =>
-                <div >
-                        <div style={{ padding: "5px", margin: "5px", border: "1px solid grey" }}>
+                <ul >
+                    <li style={{ listStyle: "none"}}>
+                        <div style={{ padding: "5px", margin: "5px", border: "1px solid grey" }} key={p.id}>
                             <p><strong>{p.title}</strong></p>
                             <p>{p.description}</p>
-                            <div style={{align: "right"}}>
-                                <button class="vote-button" onClick={handleVoteClick}>Vote for this project</button>
+                            <div style={{ align: "right" }}>
+                                <button className="vote-button" onClick={handleVoteClick}>Vote for this project</button>
                             </div>
                         </div>
-                </div>)}
+                    </li>
+                </ul>)}
         </div>
     );
 }
