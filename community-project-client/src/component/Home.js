10614222c4aec5708f726037face2c8bc81fd9a1
Aileen Gallacher <aileen.gallacher@tafensw.edu.au>
2020-07-29 10:05:51 +1000

    Added homepage content and app.css styling for homepage

--- community-project-client/src/component/Home.js
+++ community-project-client/src/component/Home.js
@@ -4,10 +4,27 @@ export default function Home() {
 
 
 
-    return(
-        <div className="home-container">
-            <div className="generation-fund-image">
-            <img src="https://images.squarespace-cdn.com/content/v1/5cb7367811f7845c98914b0c/1563339583705-0U6MD2I8ORCBTELZOHIQ/ke17ZwdGBToddI8pDm48kK1AqiaHV392C2jbmBFHILcUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKc8cWEfJifD5izkQIyQbo5BapZR1Rl1PqinRmnXXUb4OWCylY3WwPA9oq4hmJL_3iA/Vote+banner+thin.png" alt="Homepage community banner"></img>
+    return (
+        <div className="home-container" >
+            <div className="generation-fund-image" style={{align:'center'}}>
+                <img src="https://images.squarespace-cdn.com/content/v1/5cb7367811f7845c98914b0c/1563339583705-0U6MD2I8ORCBTELZOHIQ/ke17ZwdGBToddI8pDm48kK1AqiaHV392C2jbmBFHILcUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKc8cWEfJifD5izkQIyQbo5BapZR1Rl1PqinRmnXXUb4OWCylY3WwPA9oq4hmJL_3iA/Vote+banner+thin.png" alt="Homepage community banner" height='250px'></img>
+            </div>
+            <br />
+            <div className="page-content" style={{padding:'10px', margin:'10px'}}>
+                <p>Each year the NSW government provides funding through the NSW Generations Fund for projects in the local community.
+                    Submissions for projects are welcomed from members of local NSW communities
+                </p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                <p></p>
+                </div>
+            <div class='row'>
+                <div class='column'>column 1</div>
+                <div class='column'>column 2</div>
+                <div class='column'>column 3</div>
             </div>
         </div>
     );
